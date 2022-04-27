c2a581bee20934f59f8cd27472f7c49eb3889e1c
Filippo Ranza <filipporanza@gmail.com>
2022-04-27 09:13:21 +0200

    remove gnuplot support

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ mod build_graph;
 mod float_table;
 mod intersections;
 mod make_curves;
-mod make_figure;
 mod node_locations;
 mod rand_utils;
 
@@ -42,15 +41,6 @@ enum PlotFormat {
     PlotPng,
 }
 
-#[derive(Deserialize)]
-struct PlotInfo {
-    format: PlotFormat,
-    width: u32,
-    height: u32,
-    name: String,
-    title: Option<String>,
-}
-
 const DEFAULT_TRIALS: usize = 100;
 const DEFAULT_COUNT: usize = 1;
 
@@ -74,7 +64,6 @@ struct Configuration {
     #[serde(default = "get_default_count")]
     count: usize,
     save_option: Option<SaveFormat>,
-    plot_option: Option<PlotInfo>,
 }
 
 impl Configuration {
@@ -164,47 +153,7 @@ fn save_if_required(
     Ok(())
 }
 
-fn plot<F>(name: &str, id: usize, ext: &str, width: u32, height: u32, mut f: F) -> MResult<()>
-where
-    F: FnMut(&str, u32, u32) -> Result<(), gnuplot::GnuplotInitError>,
-{
-    let name = format!("{name}-{id}.{ext}");
-    f(&name, width, height)?;
-    Ok(())
-}
 
-fn plot_if_required(
-    net: &build_graph::Network,
-    format: &Option<PlotInfo>,
-    id: usize,
-) -> MResult<()> {
-    if let Some(format) = format {
-        let mut fig = make_figure::make_figure(net);
-        if let Some(title) = &format.title {
-            fig.set_title(title);
-        }
-
-        match format.format {
-            PlotFormat::PlotEps => plot(
-                &format.name,
-                id,
-                "eps",
-                format.width,
-                format.height,
-                |n, w, h| fig.save_to_eps(n, w, h),
-            )?,
-            PlotFormat::PlotPng => plot(
-                &format.name,
-                id,
-                "png",
-                format.width,
-                format.height,
-                |n, w, h| fig.save_to_png(n, w, h),
-            )?,
-        }
-    }
-    Ok(())
-}
 
 fn build_random_instance(config: &Configuration, id: usize) -> MResult<()> {
     let factory_config = config.make_factory_config();
@@ -212,7 +161,6 @@ fn build_random_instance(config: &Configuration, id: usize) -> MResult<()> {
     if let Some(network) = build_network(&factory_config, config.trials, &config.lines) {
         println!("{:?}", dot::Dot::new(&network.graph));
         save_if_required(&network, &config.save_option, id)?;
-        plot_if_required(&network, &config.plot_option, id)?;
     } else {
         println!(
             "Error: system did not generate a Connected Random Network in {} trials",
